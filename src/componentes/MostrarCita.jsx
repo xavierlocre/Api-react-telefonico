54303f549ed9492be543723c200cda3af3ad9b43
Viktor.A <viktor.antonyshyn@gmail.com>
2023-08-30 12:07:39 +0200

    Added codigo para MostrarCita

--- src/componentes/MostrarCita.jsx
+++ src/componentes/MostrarCita.jsx
@@ -8,7 +8,8 @@ import './MostrarCita.css'
 const url = "http://localhost:8080/api/v1/citas"
 const MostrarCita = () => {
     const [cita, setCita] = useState([])
-    useEffect (() => {getAllCitas() 
+    useEffect(() => {
+        getAllCitas()
     }, [])
 
     const getAllCitas = async () => {
@@ -17,6 +18,28 @@ const MostrarCita = () => {
         console.log(data)
         setCita(data)
     }
+
+    const handleDelete = async (id) => {
+        await axios.delete(`${url}/${id}`);
+        setCita((prevCita) => prevCita.filter((cita) => cita.id !== id));
+        alert(`ATENCION! ELIMINANDO CITA NUMERO ${id}`);
+    };
+
+    return (
+        <>
+            <div className="contenedor-cita" key={cita.id}>
+                <p> {cita.nameCita} </p>
+                <p> {cita.nameUser} </p>
+                <p> {cita.fecha} </p>
+                <p> {cita.hora} </p>
+            </div>
+            <div>
+                <NavLink to={`/edit/${cita.id}`}><button className="buttons">Editar Cita</button></NavLink>
+                <button className="buttons" onClick={() => handleDelete(cita.id)}>Eliminar Cita</button>
+                
+            </div>
+        </>
+    )
 }
 
 
