b3a1617cb6bbde63072a65275387b25b77134b28
Viktor.A <viktor.antonyshyn@gmail.com>
2023-08-31 19:17:32 +0200

    !!!Funciona pagina CrearCita

--- src/componentes/CrearCita.jsx
+++ src/componentes/CrearCita.jsx
@@ -13,11 +13,37 @@ const CrearCita = () => {
     const navigate = useNavigate()
     const store = async (e) => {
         e.preventDefault()
-        await axios.post(url, {nameCita: nameCita, nameUser: nameUser, fecha: fecha, hora: hora})
+        await axios.post(url, { nameCita: nameCita, nameUser: nameUser, fecha: fecha, hora: hora })
         navigate("/")
     }
 
-
-
+    return (
+        <>
+            <div className="all-container">
+                <div className="containet-create">
+                    <h3>Crear nueva cita</h3>
+                    <form onSubmit={store}>
+                        <div>
+                            <form>Nombre de cita</form>
+                            <input type="text" value={nameCita} onChange={(e) => setNameCita(e.target.value)} />
+                        </div>
+                        <div>
+                            <form>Nombre de usuario</form>
+                            <input type="text" value={nameUser} onChange={(e) => setNameUser(e.target.value)} />
+                        </div>
+                        <div>
+                            <form>Fecha</form>
+                            <input type="text" value={fecha} onChange={(e) => setFecha(e.target.value)} />
+                        </div>
+                        <div>
+                            <form>Hora</form>
+                            <input type="text" value={hora} onChange={(e) => setHora(e.target.value)} />
+                        </div>
+                        <button className="buttons">Añadir</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 export default CrearCita 
